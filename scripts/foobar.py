8212974ad43e716827f9c6346ef6e6a3c1dfe1b2
Lucas Schiavo <lucasslongoschiavo@gmail.com>
2024-08-04 16:45:07 -0300

    some testing for the application event loop

--- scripts/foobar.py
+++ scripts/foobar.py
@@ -2,21 +2,44 @@ import time
 
 from rich.table import Table
 from rich.live import Live
+from rich.layout import Layout
+import keyboard
 
-file = open("./scripts/out.txt", "r", encoding="utf8")
-file.readline()
+from models.track import Track
 
-info = list()
-for _ in range(500):
-  info.append(file.readline().split("-"))
+tracks = [Track(1, "foo", 10, 50, False, "-"),
+          Track(2, "bar", 10, 50, False, "-"),
+          Track(3, "quux", 10, 50, False, "-"),
+          Track(4, "baz", 10, 50, False, "-")
+        ]
+table = Table(expand=True)
 
-table = Table(title="My tracks")
-table.add_column("Playlist name", style="cyan", no_wrap=True)
-table.add_column("Track name", style="green", no_wrap=True)
+table.add_column("Track ID",ratio=1)
+table.add_column("Name",ratio=6)
+table.add_column("Popularity",ratio=1)
+table.add_column("Duration",ratio=1)
+table.add_column("Explicit",ratio=1)
+table.add_column("External URLs",ratio=6)
 
-for i in info:
-  table.add_row(i[0], i[1])
+for track in tracks:
+  table.add_row(f"{track.id}",f"{track.name}",f"{track.popularity}",f"{track.duration}",f"{track.explicit}", f"{track.external_URLs}")
 
-console = Console()
+def make_layout():
+  layout = Layout()
+  layout.split(
+    Layout(name="header", ratio=1),
+    Layout(name="body", ratio=5)
+  )
+  return layout
 
-console.print(table)
+layout = make_layout()
+layout['body'].update(table)
+
+with Live(layout, refresh_per_second=4) as live:
+  while True:
+    time.sleep(0.2)
+    event = keyboard.read_event()
+    if event.event_type == keyboard.KEY_DOWN and event.name == 'q':
+      break
+
+    table.add_row(event.name)
